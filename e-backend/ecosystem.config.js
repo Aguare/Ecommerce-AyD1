23edbbbb32641f33594e96c334319dcae3a2aa89
Marcos Aguare <marcosy300@gmail.com>
2024-08-14 00:35:07 -0600

    Init estructure project

---
+++ e-backend/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: "msAdmin",
+            script: "./msAdmin/index.js",
+            watch: true,
+            env_production: {
+                PORT: 3001,
+				JWT_SECRET: "zl_frperg_xrl_cebcreqri",
+				NODE_ENV: "production"
+            },
+        }
+    ],
+};
