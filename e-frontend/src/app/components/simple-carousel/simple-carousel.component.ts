96ec86a0bae101d762d6ac101db2263909442f26
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-08-21 16:42:59 -0600

    Agregamos el boton para ver el producto y creamos el componente de simpleCarrousel

---
+++ e-frontend/src/app/components/simple-carousel/simple-carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './simple-carousel.component.html',
+  styleUrl: './simple-carousel.component.scss'
+})
+export class SimpleCarouselComponent {
+
+}
