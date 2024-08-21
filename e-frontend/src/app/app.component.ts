c3776e9c3b52dcbdcfee7a961f3420b0d7846d7e
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-08-20 20:08:52 -0600

    Creamos el componente de carousel

--- e-frontend/src/app/app.component.ts
+++ e-frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PruebaMaterialComponent } from "./prueba-material/prueba-material.component";
+import { ProductCarouselComponent } from './components/product-carousel/product-carousel.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PruebaMaterialComponent],
+  imports: [RouterOutlet, PruebaMaterialComponent, ProductCarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
