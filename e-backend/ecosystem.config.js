0aa842f36fc5a6a7de591997a28b0c6ce57c8f1e
9601dani <erickmorales201930699@cunoc.edu.gt>
2024-08-21 00:17:54 -0600

    add msImg Upload

--- e-backend/ecosystem.config.js
+++ e-backend/ecosystem.config.js
@@ -12,5 +12,17 @@ module.exports = {
 				NODE_ENV: "production",
 			},
 		},
+		{
+			name: "msImg",
+			script: "./msImg/index.js",
+			watch: true,
+			env_production: {
+				PORT: 3002,
+				JWT_SECRET: "zl_frperg_xrl_cebcreqri",
+				NODE_ENV: "production",
+				MAX_FILE_SIZE: '5000000',
+				MAX_FILES: '10',
+			},
+		},
 	],
 };
---
+++ e-backend/msImg/index.js
@@ -0,0 +1,42 @@
+require('dotenv').config({ path: './config/dev.env' });
+const express = require("express");
+const app = express();
+const router = require("./routes/router");
+const log4js = require("log4js");
+const logger = log4js.getLogger();
+const PORT = process.env.PORT || 3002;
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Content-Length, Content-Disposition, Accept, Access-Control-Allow-Request-Method"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
+});
+
+app.use(router)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+if (process.env.NODE_ENV === "production") {
+	logger.level = "trace";
+	logger.trace(`= = = = = = PROJECT IN PRODUCTION = = = = = =`);
+} else {
+	logger.level = "trace";
+	logger.trace(`= = = = = = PROJECT IN DEVELOPMENT = = = = = =`);
+}
+
+app.listen(PORT, () => {
+	logger.level = "info";
+	logger.info(`App listening at http://localhost:${PORT}`);
+});
+
+//test
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
