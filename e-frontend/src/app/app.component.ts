7492a5f71da5308b4cce2ff33325b5c70df4a3a8
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-08-21 18:13:03 -0600

    Mejoramos el css de las tarjetas del carrousel

--- e-frontend/src/app/app.component.ts
+++ e-frontend/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PruebaMaterialComponent } from "./prueba-material/prueba-material.component";
 import { ProductCarouselComponent } from './components/product-carousel/product-carousel.component';
+import { SimpleCarouselComponent } from './components/simple-carousel/simple-carousel.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PruebaMaterialComponent, ProductCarouselComponent],
+  imports: [RouterOutlet, PruebaMaterialComponent, ProductCarouselComponent, SimpleCarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
