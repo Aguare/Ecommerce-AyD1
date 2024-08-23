dfefb6beab165b7f00bc962dde6343c264f8cac4
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-08-23 11:41:03 -0600

    fix bugs and add label of discount in product-card

--- e-frontend/src/app/components/commons/home/home.component.ts
+++ e-frontend/src/app/components/commons/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductCarouselComponent } from '../../product-carousel/product-carousel.component';
+import { SimpleCarouselComponent } from '../../simple-carousel/simple-carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductCarouselComponent, SimpleCarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
