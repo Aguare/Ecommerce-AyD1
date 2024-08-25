9867af33ea5c8c34aeba533a39ab7b203e2b1e76
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-08-24 19:27:45 -0600

    create navbar for guest and users

--- e-frontend/src/app/components/commons/home/home.component.ts
+++ e-frontend/src/app/components/commons/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { NavbarGuestComponent } from '../navbar-guest/navbar-guest.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, NavbarGuestComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ e-frontend/src/app/components/commons/navbar-guest/navbar-guest.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-guest',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar-guest.component.html',
+  styleUrl: './navbar-guest.component.scss'
+})
+export class NavbarGuestComponent {
+
+}
