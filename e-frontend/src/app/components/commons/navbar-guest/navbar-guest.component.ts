cfe32f6283f8bcaa4598339e4bdf4eb7e819c7bc
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-08-26 18:31:17 -0600

    add forms to change information of store

--- e-frontend/src/app/components/commons/navbar-guest/navbar-guest.component.ts
+++ e-frontend/src/app/components/commons/navbar-guest/navbar-guest.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-guest',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar-guest.component.html',
   styleUrl: './navbar-guest.component.scss'
 })
