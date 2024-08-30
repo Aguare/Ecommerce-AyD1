dfe9d91ccbcf43aa9ad53a0d65e143a86bdc2e0f
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-08-29 20:30:08 -0600

    Agregamos la conexion y el controlador para obtener las pages

--- e-frontend/src/app/components/commons/home/home.component.ts
+++ e-frontend/src/app/components/commons/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductCarouselComponent } from '../../product-carousel/product-carousel.component';
 import { SimpleCarouselComponent } from '../../simple-carousel/simple-carousel.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { NavbarGuestComponent } from '../navbar-guest/navbar-guest.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductCarouselComponent, SimpleCarouselComponent],
+  imports: [ProductCarouselComponent, SimpleCarouselComponent, NavbarComponent, NavbarGuestComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
