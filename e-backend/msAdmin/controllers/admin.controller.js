445bb1a75a57060cd6aa1d82e801fb0f0714e555
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-08-30 08:42:10 -0600

    add controller to gerPages  by id

--- e-backend/msAdmin/controllers/admin.controller.js
+++ e-backend/msAdmin/controllers/admin.controller.js
@@ -9,16 +9,12 @@ adminController.getPages = async (req, res) => {
 	let connection;
     try {
         const { id } = req.body;
-
-        // Obtener la conexión
         connection = await getConnection();
 
-        // Validación básica del ID
         if (!id) {
             return res.status(400).send({ message: 'El ID es requerido.' });
         }
 
-        // Consulta SQL
         const queryMoney = `
             SELECT p.id, p.name as pageName, p.direction, p.isAvailable, m.name as moduleName
             FROM user_has_role uhs
@@ -28,17 +24,13 @@ adminController.getPages = async (req, res) => {
             LEFT JOIN module m ON m.id = p.FK_Module
             WHERE uhs.FK_User = ?`;
 
-        // Ejecutar la consulta
         const result = await connection.query(queryMoney, [id]);
 
-        // Enviar el resultado al cliente
         res.status(200).send({ result });
 
     } catch (error) {
-        console.error("Error en getPages:", error);
         res.status(500).send({ message: "Error al obtener las páginas.", error: error.message });
     } finally {
-        // Asegurarse de liberar la conexión
         if (connection) {
             connection.release();
         }
