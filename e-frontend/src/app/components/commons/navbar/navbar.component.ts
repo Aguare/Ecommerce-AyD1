353848dcc7c15aac025afc5368a36ab735d7721e
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-08-30 12:10:58 -0600

    Update navbar with pages by BD, an adminService was created, and delete deprecated code

--- e-frontend/src/app/components/commons/navbar/navbar.component.ts
+++ e-frontend/src/app/components/commons/navbar/navbar.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { AdminService, Page, PagesResponse } from '../../../services/admin.service';
-import { error, log } from 'console';
 
 interface MenuItem {
   module: string;
@@ -25,23 +24,22 @@ interface PageItem {
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent implements OnInit {
-  isLoggedIn = false;
-  rol: string = '';
-  menuItems: MenuItem[] = [];
-  isClient = false;
+
+  isClient = true;
   pages: Page[] = [];
   pagesNavBar: MenuItem[] = []
 
   constructor(private adminService: AdminService) {}
 
   ngOnInit(): void {
-    const userId = 1; // El ID del usuario para enviar en la petición
-    this.adminService.getPages(1).subscribe({
+    const userId = 2; 
+
+    // Function to get pages of user by his id
+    this.adminService.getPages(userId).subscribe({
       next: (res: PagesResponse) => {
         this.pages = res.result;
         this.pagesNavBar = this.groupPagesByModule(this.pages);
-        console.log(this.pagesNavBar);
-        
+      
       },
       error: (err: any) => {
         console.log('Error:', err);
@@ -49,6 +47,12 @@ export class NavbarComponent implements OnInit {
     });
   }
 
+
+/**
+ * Method to convert pages to navbarMenu
+ * @param pages 
+ * @returns 
+ */
   groupPagesByModule(pages: Page[]): MenuItem[] {
     const groupedPages: { [key: string]: PageItem[] } = {};
   
@@ -68,57 +72,5 @@ export class NavbarComponent implements OnInit {
       pages: groupedPages[moduleName],
     }));
   }
-  buildMenu(): void {
-    this.menuItems = [...this.getDynamicItems()];
-  }
 
-  /** Ejemplo para probar los menuItems */
-  getDynamicItems(): any[] {
-    const dynamicItems = [];
-
-    if (this.rol === 'Administrador') {
-      this.isClient = false;
-      dynamicItems.push(
-        {
-          name: 'Productos',
-          link: '---',
-          items: [
-            { name: 'Agregar Producto', link: '/addProduct' },
-            { name: 'Ver Productos', link: '/products' },
-          ],
-        },
-        {
-          name: 'Pedidos',
-          link: '----',
-          items: [{ name: 'Gestionar Pedidos', link: '/orders' }],
-        },
-        {
-          name: 'Reportes',
-          link: '----',
-          items: [
-            { name: 'Dashboard', link: '/dashboard' },
-            { name: 'Reportes Perzonalizados', link: '/personal-reports' },
-          ],
-        },
-        {
-          name: 'Tienda',
-          link: '----',
-          items: [
-            { name: 'Configuracion', link: '/store-config' },
-            { name: 'Facturacion', link: '/store-billing' },
-          ],
-        },
-        {
-          name: 'Ayudantes',
-          link: '----',
-          items: [
-            { name: 'Crear Ayudante', link: '/create-assistant' },
-            { name: 'Permisos', link: '/assistants' },
-          ],
-        }
-      );
-    }
-
-    return dynamicItems;
-  }
 }
