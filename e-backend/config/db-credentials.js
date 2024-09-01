6bfecad5f49c5f4bb766c2eaddc7450c0e8ab1eb
Aguare <marcosy300@gmail.com>
2024-09-01 01:19:26 -0600

    Drop files for endpoints

--- e-backend/config/db-credentials.js
+++
@@ -1,8 +0,0 @@
-const data = {
-    host: 'ecommercebd.cla2eggicw1h.us-east-1.rds.amazonaws.com',
-    user:'desarrollador',
-    password:'xdfAyD1_!',
-    database:'ecommerce_ayd'
-}
-
-module.exports = data;
