a9481dc44f4a99205037c9f704ea1c7455364ee5
9601dani <erickmorales201930699@cunoc.edu.gt>
2024-09-02 14:32:48 -0600

    fix: location/paths where images are saved

--- e-backend/ecosystem.config.js
+++ e-backend/ecosystem.config.js
@@ -1,11 +1,16 @@
 /** @format */
 
+const paths = [
+	"./msImg/public/*",
+	"./msImg/public/**",
+]
+
 module.exports = {
 	apps: [
 		{
 			name: "msAdmin",
 			script: "./msAdmin/index.js",
-			watch: true,
+			ignore_watch: paths,
 			env_production: {
 				PORT: 3001,
 				JWT_SECRET: "zl_frperg_xrl_cebcreqri",
@@ -15,19 +20,19 @@ module.exports = {
 		{
 			name: "msImg",
 			script: "./msImg/index.js",
-			watch: true,
+			ignore_watch: paths,
 			env_production: {
 				PORT: 3002,
 				JWT_SECRET: "zl_frperg_xrl_cebcreqri",
 				NODE_ENV: "production",
 				MAX_FILE_SIZE: '5000000',
-				MAX_FILES: '10',
+				MAX_FILES: '10'
 			},
 		},
 		{
 			name: "msCustomer",
 			script: "./msCustomer/index.js",
-			watch: true,
+			ignore_watch: paths,
 			env_production: {
 				PORT: 3003,
 				NODE_ENV: "production"
