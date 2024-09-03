f6193216ab77b611d07ded88af515c37cffea094
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-09-03 00:07:45 -0600

    Add personal style to bulma and improve desing on home

---
+++ e-frontend/src/app/components/card-carrousel/card-carrousel.component.ts
@@ -0,0 +1,35 @@
+import { AfterViewInit, Component } from '@angular/core';
+
+import Splide from '@splidejs/splide';
+
+@Component({
+  selector: 'app-card-carrousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-carrousel.component.html',
+  styleUrl: './card-carrousel.component.scss'
+})
+export class CardCarrouselComponent implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+    new Splide('#image-carousel', {
+      type: 'loop',
+      perPage: 3,
+      perMove: 1,
+      gap: '20px',
+      padding: {
+        right: '10px',
+        left: '10px',
+      },
+      autoplay: true,
+      breakpoints: {
+        1024: {
+          perPage: 2,
+        },
+        768: {
+          perPage: 1,
+        },
+      },
+    }).mount();
+  }
+}
