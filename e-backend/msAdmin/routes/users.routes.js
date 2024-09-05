893b97eeccdfec5f44c6e407eafaa9748f16212e
Isaac03483 <levihernandez2501@gmail.com>
2024-09-04 22:42:19 -0600

    Fixing bug, updating routes for user

--- e-backend/msAdmin/routes/users.routes.js
+++ e-backend/msAdmin/routes/users.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usersController = require('../controllers/users.controller')
 
 router.post('/login', usersController.login)
+router.put('/update/user/information/:id', usersController.updateUserInformation)
 
 module.exports = router;
