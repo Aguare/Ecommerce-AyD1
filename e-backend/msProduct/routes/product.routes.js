e42b2712d9ba3a63afdb12005af64d5691b0dac0
Oscar de León Urizar <oscar99deleon19@gmail.com>
2024-09-06 13:22:20 -0600

    Create dinamic carrousells by category

--- e-backend/msProduct/routes/product.routes.js
+++ e-backend/msProduct/routes/product.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/product.controller')
 
 router.post('/getProductsByCategory', productController.getProductsByCategory);
+router.get('/getProductsWithCategory', productController.getProductsWithCategory);
 router.post('/getProductsForCart', productController.getProductsForCart);
 
 module.exports = router;
--- e-frontend/src/app/components/category-carrousell/category-carrousell.component.ts
+++ e-frontend/src/app/components/category-carrousell/category-carrousell.component.ts
@@ -6,132 +6,97 @@ import Splide from '@splidejs/splide';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../../services/product.service';
 import { ImagePipe } from '../../pipes/image.pipe';
-import { ProductCardComponent } from "../product-card/product-card.component";
+import { ProductCardComponent } from '../product-card/product-card.component';
+
+
+export interface ProductCart {
+  category: string;
+  products: Product[];
+}
 
 @Component({
   selector: 'app-category-carrousell',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, CommonModule, ImagePipe, ProductCardComponent],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    CommonModule,
+    ImagePipe,
+    ProductCardComponent,
+  ],
   templateUrl: './category-carrousell.component.html',
-  styleUrl: './category-carrousell.component.scss'
+  styleUrl: './category-carrousell.component.scss',
 })
 export class CategoryCarrousellComponent implements AfterViewInit {
+  products: Product[] = [];
+  productsCart: ProductCart[] = [];
 
-  products1: Product[] = []
-  products2: Product[] = []
-  products3: Product[] = []
+  constructor(private productService: ProductService) {}
 
-  title1 = "Tecnologia"
-  title2 = "Hogar"
-  title3 = "Academico"
-
-  constructor(private productService: ProductService){}
-
- ngOnInit(): void {
-  this.productService.getProductsByCategory(this.title1).subscribe({
-    next: (res: Product[]) => {
-      this.products1 = res;  
-      this.products1 = this.products1.slice(0,10);     
-    },
-    error: (err: any) => {
-      console.log('Error:', err);
-    },
-  });
-  
-  this.productService.getProductsByCategory(this.title2).subscribe({
-    next: (res: Product[]) => {
-      this.products2 = res;      
-      this.products2 = this.products2.slice(0,10);
-    },
-    error: (err: any) => {
-      console.log('Error:', err);
-    },
-  });
-  
-  this.productService.getProductsByCategory(this.title3).subscribe({
-    next: (res: Product[]) => {
-      this.products3 = res;     
-      this.products3 = this.products3.slice(0,10); 
-    },
-    error: (err: any) => {
-      console.log('Error:', err);
-    },
-  });
- }
-
- ngAfterViewInit(): void {
-  if (typeof document !== 'undefined') {
-    const splide1 = new Splide('#category-carrousel1', {
-      type: 'loop',
-      perPage: 5,
-      perMove: 1,
-      gap: '10px',
-      padding: {
-        right: '0',
-        left: '0',
+  ngOnInit(): void {
+    this.productService.getProductsWithCategory().subscribe({
+      next: (res: Product[]) => {
+        this.products = res;
+        this.productsCart = this.groupByCategory(this.products);
       },
-      autoplay: true,
-      breakpoints: {
-        1024: {
-          perPage: 2, 
-          gap: '8px',
-        },
-        768: {
-          perPage: 1, 
-          gap: '5px',
-        },
+      error: (err: any) => {
+        console.log('Error:', err);
       },
     });
-
-    splide1.mount();
-    
-    const splide2 = new Splide('#category-carrousel2', {
-      type: 'loop',
-      perPage: 4,
-      perMove: 1,
-      gap: '10px',
-      padding: {
-        right: '0',
-        left: '0',
-      },
-      autoplay: true,
-      breakpoints: {
-        1024: {
-          perPage: 2, 
-          gap: '8px',
-        },
-        768: {
-          perPage: 1, 
-          gap: '5px',
-        },
-      },
-    });
-
-    splide2.mount();
-
-    const splide3 = new Splide('#category-carrousel3', {
-      type: 'loop',
-      perPage: 4,
-      perMove: 1,
-      gap: '10px',
-      padding: {
-        right: '0',
-        left: '0',
-      },
-      autoplay: true,
-      breakpoints: {
-        1024: {
-          perPage: 2, 
-          gap: '8px',
-        },
-        768: {
-          perPage: 1, 
-          gap: '5px',
-        },
-      },
-    });
-
-    splide3.mount();
   }
- }
+
+
+  ngAfterViewInit(): void {
+    setTimeout(()=>{
+      if (typeof document !== 'undefined') {
+        this.productsCart.forEach((cart) => {         
+          const splideId = '#category-carrousel-' + cart.category;
+  
+          const splide = new Splide(splideId, {
+            type: 'loop',
+            perPage: 5,
+            perMove: 1,
+            gap: '10px',
+            padding: {
+              right: '0',
+              left: '0',
+            },
+            autoplay: true,
+            breakpoints: {
+              1024: {
+                perPage: 3,
+                gap: '8px',
+              },
+              768: {
+                perPage: 1,
+                gap: '5px',
+              },
+            },
+          });
+          splide.mount();
+        });
+      }
+
+    },500)
+  }
+
+
+  groupByCategory = (products: Product[]): ProductCart[] => {
+    const groupedProducts: { [key: string]: ProductCart } = {};
+
+    products.forEach((product) => {
+      const { category } = product;
+
+      if (!groupedProducts[category]) {
+        groupedProducts[category] = {
+          category: category,
+          products: [],
+        };
+      }
+
+      groupedProducts[category].products.push(product);
+    });
+
+    return Object.values(groupedProducts);
+  };
 }
