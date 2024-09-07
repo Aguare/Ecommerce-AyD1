4cf223558690b1b78d0d423829aeed219313b896
9601dani <erickmorales201930699@cunoc.edu.gt>
2024-09-06 21:27:54 -0600

    fix: middleware

--- e-backend/middleware/verifytoken.middleware.js
+++ e-backend/middleware/verifytoken.middleware.js
@@ -1,45 +1,74 @@
-const getConnection = require('../db/db.js');
+/** @format */
+
+const getConnection = require("../db/db");
 const controller = {};
+const jwt = require("jsonwebtoken");
 
 controller.verifyToken = async (req, res, next) => {
-      const { id, auth_token } = req.headers;
+	const bearerHeader = req.headers["authorization"];
+	console.log("bearerHeader", bearerHeader);
 
-      let connec;
-        try {
-            conn = await getConnection();
+	if (!bearerHeader) {
+		return res.status(403).send({ message: "No token provided" });
+	}
 
-            if (!id || !auth_token) {
-                return res.status(400).send( {message: 'El usuario y el token son requeridos'});
-            }
+	const token = bearerHeader.split(" ")[1];
+	console.log("token", token);
 
-            const query = `SELECT * FROM user WHERE id = ? AND auth_token = ?`;
-            const result = await conn.query(query, [id, auth_token]);
+	if (!token) {
+		return res.status(403).send({ message: "No token provided" });
+	}
 
-            if (result.length === 0) {
-                const query = `SELECT * FROM user WHERE id = ?`;
-                const result = await conn.query(query, [id]);
-                if (result.length === 0) {
-                    return res.status(404).send({message: 'Usuario no encontrado'});
-                }
-                return res.status(401).send({message: 'Token inválido'});
-            }
+	let connec;
 
-            const date = new Date();
-                const tokenExpirationDate = new Date(result[0].auth_token_expired);
-                if (date > tokenExpirationDate) {
-                    return res.status(403).send({ message: 'El token ha expirado' });
-                }
-            next();
+	try {
+		connec = await getConnection();
+		const query = "SELECT key_value FROM company_settings WHERE key_name = 'session_key';";
+		const result = await connec.query(query);
 
-        }catch (error) {
-            console.log(error);
-            return res.status(500).send('Error en la conexión a la base de datos');
-        } finally {
-            if (conn) {
-                conn.end();
-            }
-        }
+		if (result.length === 0) {
+			return res.status(400).send({ message: "Error interno al iniciar la sesion" });
+		}
 
+		jwtKey = result[0].key_value;
+		jwt.verify(token, jwtKey, async (err, decoded) => {
+			if (err) {
+				if (err.name === "TokenExpiredError") {
+					return res.status(401).send({ message: "El token ha expirado. Por favor, inicia sesión de nuevo." });
+				} else {
+					return res.status(403).send({ message: "Token no válido" });
+				}
+			}
+
+			const sql = `SELECT * from user WHERE auth_token = ? AND id = ?`;
+			const result_token = await connec.query(sql, [token, decoded.id]);
+
+			if (result_token.length === 0) {
+				return res.status(403).send({ message: "Token no válido" });
+			}
+
+			const dateNow = new Date();
+			const queryExpired = "SELECT auth_token_expired FROM user WHERE id = ?;";
+			const resultExpired = await connec.query(queryExpired, [decoded.id]);
+
+			if (resultExpired.length === 0) {
+				return res.status(403).send({ message: "Token no válido" });
+			}
+
+			const expired = new Date(resultExpired[0].auth_token_expired);
+			if (dateNow > expired) {
+				return res.status(403).send({ message: "Token expirado" });
+			}
+			next();
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({ message: "Error al obtener la información", error: error.message });
+	} finally {
+		if (connec) {
+			connec.end();
+		}
+	}
 };
 
 module.exports = controller;
